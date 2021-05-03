01a5cfa500f24ec2c5199ae7b110994fb4300bf8
Ana Ceprnja <aceprnja@iun.edu>
2021-05-02 20:28:38 -0600

    public statis

--- practice.java
+++ practice.java
@@ -1,3 +1,16 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
+class Main
+{
+  
+    public static int findKthLargest(List<Integer> ints, int k)
+    {
+  
+        PriorityQueue<Integer> pq = new PriorityQueue<>(ints.subList(0, k));
+ 
+      
+        for (int i = k; i < ints.size(); i++)
+        {
+
+     
