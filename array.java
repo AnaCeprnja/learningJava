a21039d33b1087032515cb65ebd4efb99c2c2bc6
Ana Ceprnja <aceprnja@iun.edu>
2021-05-12 21:10:38 -0600

    array java

--- array.java
+++ array.java
@@ -1,15 +1,3 @@
-import java.util.ArrayList;
-
-//making a list of colors
-public class Color {
-    public static void colors(String[] args) {
-        ArrayList<String> cars = new ArrayList<String>();
-        color.add("red");
-        color.add("green");
-        color.add("pink");
-        color.add("blue");
-        System.out.println(color);
-    }
-}
-
-// error?
+String[] colors = {"black", "green", "blue", "red"};
+colors[0] = "Opel";
+System.out.println(colors[0]);
