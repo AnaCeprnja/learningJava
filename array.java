c4a796f4bd3a45e50ee258393fe624f597f2a37a
Ana Ceprnja <aceprnja@iun.edu>
2021-05-12 21:01:43 -0600

    list of colors

---
+++ array.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+//making a list of colors
+public class Color {
+    public static void colors(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        color.add("red");
+        color.add("green");
+        color.add("pink");
+        color.add("blue");
+        System.out.println(color);
+    }
+}
