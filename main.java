2633546210b6ee3c0960ae5acdd269b95f2dc68f
Ana Ceprnja <aceprnja@iun.edu>
2021-05-12 20:55:28 -0600

    java method printing hello world

---
+++ main.java
@@ -0,0 +1,11 @@
+public class Main {
+    static void myMethod() {
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+} // printing something 3 times
